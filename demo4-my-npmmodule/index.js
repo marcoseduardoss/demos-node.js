f8f15f75decd9e35f31933e6f90a885c8e2e7d53
marcos.eduardo <marcos.eduardo@uece.br>
2020-08-19 08:19:38 +0800

    add project

---
+++ demo4-my-npmmodule/index.js
@@ -0,0 +1,23 @@
+/**
+ * PUC-Minas / Arquitetura de Sistemas Distribuídos
+ * Author: Marcos Eduardo da Silva Santos
+ * File: my-module/funcoes.js
+ * GitHub: 
+ */
+const calc = require('mathematical-functions-node-module');
+
+const x = 10;
+const y = 2;
+
+try {
+
+    console.log('a) 1 + 1 = '           + calc.add(1,1));
+    console.log('b) -123 + 123123 = '   + calc.add(-123, 123123));
+    console.log('c) 8 * 0 = '           + calc.multiply(8,0));
+    console.log('d) 1239123 * 12313 = ' + calc.multiply(1239123,12313));
+    console.log('e) 123 / -12 = '       + calc.divide(123,-12));
+    console.log('f) 313123 / 0 = '      + calc.divide(313123,0));
+
+}catch (e) {
+    console.log(e);
+}
