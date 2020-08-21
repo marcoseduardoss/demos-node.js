45afc3dd081391c3246e66a1c652cc3bc95f1fcc
marcos.eduardo <marcos.eduardo@uece.br>
2020-08-20 22:28:12 -0300

    add project

---
+++ demo7-expressjs-site/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const logMiddleware = function(req, rex, next){
+  console.log('API recebeu alguma informação...')
+  next();
+} 
+
+app.use(logMiddleware);
+
+app.use(express.static("./site"));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/pt', (req, res) => {
+  res.send('Olá Mundo!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
